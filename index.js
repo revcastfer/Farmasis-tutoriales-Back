a5a657de19302b2b788d344aef051b240e1cb048
fernando revilla <revcast@hotmail.com>
2023-06-02 17:30:30 -0500

    quitar el force

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const { conn,cargaData } = require('./src/db.js');
 const port=process.env.PORT || 3002;
 
 // Syncing all the models at once.
-conn.sync({ force: true })
+conn.sync()
 .then(() => {
   server.listen(port, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
