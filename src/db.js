eb195966bddf46a33a9b8e2483b138ddc68ed05b
fernando revilla <revcast@hotmail.com>
2023-06-07 17:54:51 -0500

    BD

--- src/db.js
+++ src/db.js
@@ -6,14 +6,14 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY,DB_DATABASE
 } = process.env;
 
-//const sequelize = new Sequelize(DB_DEPLOY, {
-//  logging: false, // set to console.log to see the raw SQL queries
-//  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//});     
-const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+});     
+//const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`, {
+//  logging: false, // set to console.log to see the raw SQL queries
+//  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//});
 
 sequelize.authenticate().then(() => {
    console.log('Connection has been established successfully.');
